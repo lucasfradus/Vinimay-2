6ae56b0d38935b576c6c2ff3ea70672871471f10
lucasfradus <lucas.fradusco@gmail.com>
2020-12-04 11:44:37 -0300

    add google tag manager

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 import Follow from './Components/Follow'
 import Inicio from './Pages/Inicio';
@@ -16,6 +18,17 @@ import {useTranslation} from "react-i18next";
 
 
 function App() {
+
+  const trackingId = "G-E6HJ4RKQEH"; 
+  ReactGA.initialize(trackingId);
+
+  const history = createBrowserHistory();
+  // Initialize google analytics page view tracking
+  history.listen(location => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  });
+
   const [t] = useTranslation('common');
 
   return (
