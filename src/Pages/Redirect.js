f1b8d97b953a3eebbab3b72389a5ae7c03e6eabe
lucasfradus <lucas.fradusco@gmail.com>
2021-02-03 15:06:43 -0300

    testing deeplinks

---
+++ src/Pages/Redirect.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { isAndroid,isIOS,osName } from 'react-device-detect';
+import styled from 'styled-components'
+import { Animated } from 'react-animated-css'
+
+import Box from '../Components/box'
+
+/*
+    if (isAndroid){
+        window.location.replace("instagram://user?username=lucasfradusco"); setTimeout(function () {
+        window.location.href = Android; }, 25);
+
+    }
+*/
+
+const Redirect = () =>{
+
+    const ios = "https://apps.apple.com/ar/app/vinimay/id1506565366";
+    const Android = "https://play.google.com/store/apps/details?id=com.vinimay.app";
+    
+    let button;
+
+    if(isAndroid){
+        button = `Sistema ${osName} - ruta STORE: ${Android}!`;
+    }else if(isIOS){
+        button = `Sistema: ${osName} - ruta STORE: ${ios}!`;
+    }else{
+        button = "browser no soportado";
+    }
+    
+
+    return (
+        <div className='pageContacto'>
+        <Box/>
+        <Animated animationIn="fadeInUp" animationOut="flipOutX" animationInDuration={600} animationOutDuration={400} isVisible={true}>
+        {button}       
+        </Animated>
+
+    </div>
+    );
+}
+
+const Display = styled.div`
+display:flex;
+position:relative;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+height:10vh;
+z-index:99999999;
+`
+
+export default Redirect;
