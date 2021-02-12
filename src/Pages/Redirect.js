7181a25a41bc5bb2da704ef6b106bfd2cfa19be6
martin444 <alejandrofarel62@gmail.com>
2021-02-12 01:09:07 -0300

    add redirect link android and ios

--- src/Pages/Redirect.js
+++ src/Pages/Redirect.js
@@ -4,22 +4,28 @@ import { isAndroid,isIOS,osName } from 'react-device-detect';
 import Box from '../Components/box'
 
 
-const Redirect = () =>{
-
+const Redirect = ({props}) =>{
     const ios = "https://apps.apple.com/ar/app/vinimay/id1506565366";
     const Android = "https://play.google.com/store/apps/details?id=com.vinimay.app";
-    
     let redirect;
-
-    if(isAndroid){
-        redirect = `Sistema ${osName} - ruta STORE: ${Android}!`;
-    }else if(isIOS){
-        redirect = `Sistema: ${osName} - ruta STORE: ${ios}!`;
-    }else{
-        redirect = "browser no soportado";
-    }
-    
-
+    useEffect(() => {
+        if(isAndroid){
+            redirect = `Sistema ${osName} - ruta STORE: ${Android}!`;
+            window.location="instagram://user?username=lucasfradusco"
+            setTimeout(() => {
+                window.location=Android
+            }, 1500);
+        }else if(isIOS){
+            window.location="instagram://user?username=lucasfradusco"
+            setTimeout(() => {
+                window.location=ios
+            }, 1500);
+            redirect = `Sistema: ${osName} - ruta STORE: ${ios}!`;
+        }else{
+            redirect = "browser no soportado";
+            console.log(window.location=Android);
+        }
+    },)
     return (
         <div className='pageContacto'>
             <Box/>
